32e186ad1b3928aa697c48195c6ece7805b88e1a
Joshua-Raphael <joshuaraphael.gayoba@hcdc.edu.ph>
2026-01-05 02:17:51 +0800

    install whitenoise

--- server/newproject/newproject/settings.py
+++ server/newproject/newproject/settings.py
@@ -66,6 +66,8 @@ INSTALLED_APPS = [
 
     # Local apps
     'api',
+
+    'whitenoise.runserver_nostatic',
 ]
 
 # if DEBUG:
@@ -184,7 +186,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
